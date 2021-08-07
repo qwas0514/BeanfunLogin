b8210fcbc206b130e1856ec700cda1ee48b7ada4
xnum <xnumtw@gmail.com>
2021-08-07 12:22:08 +0800

    try to support CSO

--- BeanfunLogin/BeanfunClient.Account.cs
+++ BeanfunLogin/BeanfunClient.Account.cs
@@ -16,8 +16,13 @@ namespace BeanfunLogin
             { return; }
 
             Regex regex;
+
+            // Do auth.
             string response = this.DownloadString("https://tw.beanfun.com/beanfun_block/auth.aspx?channel=game_zone&page_and_query=game_start.aspx%3Fservice_code_and_region%3D" + service_code + "_" + service_region + "&web_token=" + webtoken, Encoding.UTF8);
 
+            // Fetch accounts.
+            string url = "https://tw.beanfun.com/beanfun_block/game_zone/game_server_account_list.aspx?sc=" + service_code + "&sr=" + service_region + "&dt=" + GetCurrentTime(2);
+            response = this.DownloadString(url, Encoding.UTF8);
             // Add account list to ListView.
             regex = new Regex("<div id=\"(\\w+)\" sn=\"(\\d+)\" name=\"([^\"]+)\"");
             this.accountList.Clear();
